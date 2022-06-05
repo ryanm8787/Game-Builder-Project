5ab2c33ee5689918c1a863d31ad2ffec6ae16137
ryanm8787 <rmcdonald19@qub.ac.uk>
2022-06-05 18:18:48 +0100

    start environment dev: clouds added

---
+++ Src/Assets/Scripts/Environment/scr_cloud_generation.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+namespace environment
+{
+    public class scr_cloud_generation : MonoBehaviour
+    {
+        public float cloud_speed = 0;
+        public int min_frame_interval = 0;
+        public int max_frame_interval = 0;
+
+        public float min_y_start = 0.0f;
+        public float max_y_start = 0.0f;
+
+        private const int CUT_OFF_LEFT = 50; // anything < this will mean the direction is negative
+        private const int PROB_MAX = 100;
+
+        private int m_fps = -1;
+        private int m_fps_counter = 0;
+
+        private List<scr_cloud_motion> m_clouds;
+        private float m_x_start = 0;
+        private const float X_CONST = 10f;
+
+        private const float CLOUD_ALPHA_MIN = 0.5f;
+        private const float CLOUD_ALPHA_MAX = 0.75f;
+        private void Start()
+        {
+            m_clouds = new List<scr_cloud_motion>();
+            if (min_frame_interval > max_frame_interval)
+            {
+                throw new InvalidDataException("Minimum frame interval cannot be greater than max");
+            }
+        }
+        private int determine_direction(int val)
+        {
+            if(val < CUT_OFF_LEFT)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        private int generate_interval()
+        {
+            // TODO single instance please x
+            System.Random cloud_interval_generation = new System.Random();
+
+            return cloud_interval_generation.Next(min_frame_interval, max_frame_interval);
+        }
+
+        private float generate_position_y()
+        {
+            // TODO single instance please x
+            System.Random cloud_interval_generation = new System.Random();
+
+            // TODO: this is so shit me
+            return (float)cloud_interval_generation.NextDouble() * (max_y_start - min_y_start) + min_y_start;
+        }
+        
+        // TODO implement
+        private int generate_img_scale()
+        {
+            return 1;
+        }
+        
+        private float generate_cloud_speed()
+        {
+            // TODO single instance please x
+            System.Random speed_generator = new System.Random();
+            float cloud_speed_min = 0.2f * cloud_speed;
+
+            float cloud_speed_new = (float)speed_generator.NextDouble() * (cloud_speed - cloud_speed_min) + cloud_speed_min;
+
+            return cloud_speed_new;
+        }
+
+        private float generate_cloud_alpha()
+        {
+            // TODO single instance please x
+            System.Random alpha_generator = new System.Random();
+
+            float cloud_alpha_new = (float)alpha_generator.NextDouble() * (CLOUD_ALPHA_MAX - CLOUD_ALPHA_MIN) + CLOUD_ALPHA_MIN;
+
+            return cloud_alpha_new;
+        }
+
+        private void generate_cloud()
+        {
+            // these are usually shit generators but will work for our case 
+            System.Random cloud_dir_generator = new System.Random();
+            int cloud_dir_raw = cloud_dir_generator.Next(0, PROB_MAX);
+
+            int cloud_dir = determine_direction(cloud_dir_raw);
+            Debug.Log(cloud_dir);
+            float start_y = generate_position_y();
+
+            m_x_start = X_CONST * cloud_dir * -1;
+
+            // TODO: update co-ordinates.
+            scr_cloud_motion new_cloud = new scr_cloud_motion(generate_cloud_speed(), cloud_dir, m_x_start, start_y, generate_cloud_alpha());
+            m_clouds.Add(new_cloud);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(m_fps == -1)
+            {
+                // TODO: this might not work
+                m_fps = (int)(1 / Time.deltaTime);
+            }
+
+            if(m_fps_counter < 1)
+            {
+                m_fps_counter = generate_interval();
+                generate_cloud();
+            }
+            else
+            {
+                m_fps_counter--;
+            }
+
+            foreach (scr_cloud_motion cloud in m_clouds)
+            {
+                // check if cloud is dead and remove from list. If we're removing maybe use standard int i for loop
+
+                // move cloud
+                cloud.move_cloud();
+            }
+        }
+    }
+}
---
+++ Src/Assets/Scripts/Environment/scr_cloud_motion.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace environment
+{
+    public class scr_cloud_motion : MonoBehaviour
+    {
+        private float m_cloud_speed = 0.0f;
+        private float m_direction = 0.0f; // 1 = right, -1 = left 
+        private float m_start_x = 0.0f;
+        private float m_start_y = 0.0f;
+
+        private float m_alpha = 1.0f;
+
+        private const string FILE_NAME_CLOUD = "Prefab/obj_simple_cloud";
+        private GameObject m_cloud_obj;
+        public scr_cloud_motion(float cloud_speed, float direction, float start_x, float start_y, float alpha)
+        {
+            m_cloud_speed = cloud_speed;
+            m_direction = direction;
+            m_start_x = start_x;
+            m_start_y = start_y;
+            m_alpha = alpha;
+            string path = Directory.GetCurrentDirectory();
+
+            init_cloud();
+        }
+        private void init_cloud()
+        {
+            GameObject obj_type = Resources.Load(FILE_NAME_CLOUD) as GameObject;
+
+            if (obj_type == null)
+            {
+                throw new FileNotFoundException("Could not find cloud game object. \n");
+            }
+
+            Vector2 start_pos = new Vector2(m_start_x, m_start_y);
+
+            m_cloud_obj = Instantiate(obj_type, start_pos, Quaternion.identity);
+
+            var tmp = m_cloud_obj.GetComponent<SpriteRenderer>();
+            Color new_clr = m_cloud_obj.GetComponent<SpriteRenderer>().color;
+            new_clr.a = m_alpha;
+            tmp.color = new_clr;
+        }
+
+        void Update()
+        {
+            move_cloud();
+        }
+        public void move_cloud()
+        {
+            Vector3 current_pos = m_cloud_obj.transform.position;
+            current_pos.x += m_cloud_speed * m_direction;
+            m_cloud_obj.transform.position = current_pos;
+        }
+    }
+
+}
